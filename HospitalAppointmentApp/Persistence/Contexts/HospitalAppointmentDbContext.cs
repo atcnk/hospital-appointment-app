141cf491b8b762971cc0bb6be101780df2f57e72
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-06-12 14:42:53 +0300

    added: dbcontext for app

---
+++ HospitalAppointmentApp/Persistence/Contexts/HospitalAppointmentDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class HospitalAppointmentDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<AdminAction> AdminActions { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientReport> PatientReports { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=HospitalAppointmentSystem;User Id=sa;Password=YourStrongPassword123!;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Doctor>().ToTable("Doctors");
+            modelBuilder.Entity<Patient>().ToTable("Patients");
+            modelBuilder.Entity<Admin>().ToTable("Admins");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
