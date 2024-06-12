83538495056e46867544a479a7eb3d462c680c43
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-06-12 15:05:32 +0300

    added: service registrations for persistence and application layers

---
+++ HospitalAppointmentApp/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
---
+++ HospitalAppointmentApp/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Application.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<HospitalAppointmentDbContext>();
+
+            services.AddScoped<IAdminActionRepository, AdminActionRepository>();
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IDoctorScheduleRepository, DoctorScheduleRepository>();
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+            services.AddScoped<IPatientReportRepository, PatientReportRepository>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
