de3242cfa0a3852f9fd0373ef9c46583b6a9f9ef
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-06-13 12:40:17 +0300

    added: create command, response and handler for Doctor entity

---
+++ HospitalAppointmentApp/Application/Features/Doctors/Commands/Create/CreateDoctorCommand.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enums;
+using MediatR;
+
+namespace Application.Features.Doctors.Commands.Create
+{
+    public class CreateDoctorCommand : IRequest<CreateDoctorResponse>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public City City { get; set; }
+        public string Address { get; set; }
+        public string PhotoUrl { get; set; }
+        public string SpecialistLevel { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string Biography { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ HospitalAppointmentApp/Application/Features/Doctors/Commands/Create/CreateDoctorCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Doctors.Commands.Create
+{
+    public class CreateDoctorCommandHandler : IRequestHandler<CreateDoctorCommand, CreateDoctorResponse>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        private readonly IMapper _mapper;
+
+        public CreateDoctorCommandHandler(IDoctorRepository doctorRepository, IMapper mapper)
+        {
+            _doctorRepository = doctorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreateDoctorResponse> Handle(CreateDoctorCommand request, CancellationToken cancellationToken)
+        {
+            Doctor doctor = _mapper.Map<Doctor>(request);
+            await _doctorRepository.AddAsync(doctor);
+
+            CreateDoctorResponse response = _mapper.Map<CreateDoctorResponse>(doctor);
+            return response;
+        }
+    }
+}
