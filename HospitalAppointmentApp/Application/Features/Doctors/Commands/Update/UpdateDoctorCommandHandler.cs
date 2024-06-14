b7b81dbe1d17e0bcf2787593a05bc88f3e002efa
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-06-14 16:14:57 +0300

    added: update for Doctor

---
+++ HospitalAppointmentApp/Application/Features/Doctors/Commands/Update/UpdateDoctorCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Doctors.Commands.Update
+{
+    public class UpdateDoctorCommandHandler : IRequestHandler<UpdateDoctorCommand, UpdateDoctorResponse>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateDoctorCommandHandler(IDoctorRepository doctorRepository, IMapper mapper)
+        {
+            _doctorRepository = doctorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UpdateDoctorResponse> Handle(UpdateDoctorCommand request, CancellationToken cancellationToken)
+        {
+            Doctor? doctor = await _doctorRepository.GetAsync(d => d.Id == request.Id);
+
+            if (doctor is null)
+            {
+                throw new ArgumentException("Doctor is not found");
+            }
+
+            _mapper.Map(request, doctor);
+
+            await _doctorRepository.UpdateAsync(doctor);
+
+            UpdateDoctorResponse response = _mapper.Map<UpdateDoctorResponse>(doctor);
+            return response;
+        }
+    }
+}
