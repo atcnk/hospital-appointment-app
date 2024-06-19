303496cbf48e6d1a167da80d5a37274cba081c00
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-06-19 22:14:45 +0300

    Updated; DoctorsController with basic CRUD operations

--- HospitalAppointmentApp/WebAPI/Controllers/DoctorsController.cs
+++ HospitalAppointmentApp/WebAPI/Controllers/DoctorsController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Features.Doctors.Commands.Create;
+using Application.Features.Doctors.Commands.Delete;
+using Application.Features.Doctors.Commands.Update;
+using Application.Features.Doctors.Queries.GetById;
+using Application.Features.Doctors.Queries.GetList;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
@@ -6,6 +11,41 @@ namespace WebAPI.Controllers
     [ApiController]
     public class DoctorsController : BaseController
     {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateDoctorCommand command)
+        {
+            CreateDoctorResponse response = await _mediator.Send(command);
+            return Ok(response);
+        }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            GetByIdDoctorQuery query = new() { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetListDoctorQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateDoctorCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            DeleteDoctorCommand command = new () { Id = id };
+            await _mediator.Send(command);
+            return Ok("Deleted");
+        }
     }
 }
