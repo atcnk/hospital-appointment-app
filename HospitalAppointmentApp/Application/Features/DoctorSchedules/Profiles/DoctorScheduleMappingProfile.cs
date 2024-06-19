16f1e1f5ab12788bc80529f4e2058ab7dc83159d
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-06-19 21:10:06 +0300

    Added: profiles for Doctor and DoctorSchedule

---
+++ HospitalAppointmentApp/Application/Features/DoctorSchedules/Profiles/DoctorScheduleMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.DoctorSchedules.Commands.Create;
+using Application.Features.DoctorSchedules.Commands.Update;
+using Application.Features.DoctorSchedules.Queries.GetById;
+using Application.Features.DoctorSchedules.Queries.GetList;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.DoctorSchedules.Profiles
+{
+    public class DoctorScheduleMappingProfile : Profile
+    {
+        public DoctorScheduleMappingProfile()
+        {
+            CreateMap<DoctorSchedule, CreateDoctorScheduleCommand>().ReverseMap();
+            CreateMap<DoctorSchedule, CreateDoctorScheduleResponse>().ReverseMap();
+            CreateMap<DoctorSchedule, GetByIdDoctorScheduleQuery>().ReverseMap();
+            CreateMap<DoctorSchedule, GetByIdDoctorScheduleResponse>().ReverseMap();
+            CreateMap<DoctorSchedule, GetListDoctorScheduleQuery>().ReverseMap();
+            CreateMap<DoctorSchedule, GetListDoctorScheduleResponse>().ReverseMap();
+            CreateMap<DoctorSchedule, UpdateDoctorScheduleCommand>().ReverseMap();
+            CreateMap<DoctorSchedule, UpdateDoctorScheduleResponse>().ReverseMap();
+        }
+    }
+}
---
+++ HospitalAppointmentApp/Application/Features/Doctors/Profiles/DoctorMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.Doctors.Commands.Create;
+using Application.Features.Doctors.Commands.Update;
+using Application.Features.Doctors.Queries.GetById;
+using Application.Features.Doctors.Queries.GetList;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Doctors.Profiles
+{
+    public class DoctorMappingProfile : Profile
+    {
+        public DoctorMappingProfile()
+        {
+            CreateMap<Doctor, CreateDoctorCommand>().ReverseMap();
+            CreateMap<Doctor, CreateDoctorResponse>().ReverseMap();
+            CreateMap<Doctor, GetByIdDoctorQuery>().ReverseMap();
+            CreateMap<Doctor, GetByIdDoctorResponse>().ReverseMap();
+            CreateMap<Doctor, GetListDoctorQuery>().ReverseMap();
+            CreateMap<Doctor, GetListDoctorResponse>().ReverseMap();
+            CreateMap<Doctor, UpdateDoctorCommand>().ReverseMap();
+            CreateMap<Doctor, UpdateDoctorResponse>().ReverseMap();
+        }
+    }
+}
