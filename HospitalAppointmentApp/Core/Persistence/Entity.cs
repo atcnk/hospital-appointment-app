5719065d22a794a225b32b8a71b41aed18de9493
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-06-19 21:08:16 +0300

    Updated: base Entity with IsDeleted property

--- HospitalAppointmentApp/Core/Persistence/Entity.cs
+++ HospitalAppointmentApp/Core/Persistence/Entity.cs
@@ -6,6 +6,7 @@
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
+        public bool IsDeleted { get; set; }
 
         public Entity()
         {
