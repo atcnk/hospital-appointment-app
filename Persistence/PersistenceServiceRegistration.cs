b49a185f56c97c04936cdf130f863b2bdf3e12a5
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-08-03 16:04:58 +0300

    Added: service registration for persistence layer

---
+++ Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using Application.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services) 
+        {
+            services.AddDbContext<HospitalAppointDbContext>();
+
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IDoctorAvailabilityRepository, DoctorAvailabilityRepository>();
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+            services.AddScoped<IPatientReportRepository, PatientReportRepository>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
+            services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+            services.AddScoped<ISupportRequestRepository, SupportRequestRepository>();
+
+            return services;
+        }
+    }
+}
