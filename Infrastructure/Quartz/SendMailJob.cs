4bb09b445dc8c07f4b6cf648e79c3b9a25bb9cdf
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-08-03 16:20:22 +0300

    Added: Quartz to inftrastructure

---
+++ Infrastructure/Quartz/SendMailJob.cs
@@ -0,0 +1,29 @@
+﻿using Application.Repositories;
+using Core.Mailing;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace Infrastructure.Quartz
+{
+	public class SendMailJob : IJob
+	{
+		private readonly IMailService _mailService;
+		private readonly ILogger _logger;
+		private readonly IAppointmentRepository _appointmentRepository;
+
+		public SendMailJob(IMailService mailService, ILogger<SendMailJob> logger, IAppointmentRepository appointmentRepository)
+		{
+			_mailService = mailService;
+			_logger = logger;
+			_appointmentRepository = appointmentRepository;
+		}
+
+		public async Task Execute(IJobExecutionContext context)
+		{
+			var mailList = await _appointmentRepository.GetLastOneDayPatients();
+			var mailList2 = await _appointmentRepository.GetTodaysAppointments();
+			await _mailService.BookedAppointmentReminderMailAsync(mailList);
+			await _mailService.BookedAppointmentReminderToDoctorMailAsync(mailList2);
+		}
+	}
+}
