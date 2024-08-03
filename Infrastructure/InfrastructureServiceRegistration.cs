a429312876f19e3486b9afaf0aa35399a88d5057
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-08-03 16:20:38 +0300

    Added: service registration for infrastructure layer

---
+++ Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Quartz;
+using Infrastructure.SignalR.HubService;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+			services.AddQuartz(q =>
+			{
+				q.UseMicrosoftDependencyInjectionJobFactory();
+				q.ScheduleJob<SendMailJob>(trigger => trigger
+					.WithIdentity("SendRecurringMailTrigger")
+					.StartAt(DateBuilder.TodayAt(15, 53, 00)) // Gece yarısı
+					.WithSimpleSchedule(s =>
+						s.WithIntervalInHours(24) // Her 24 saatte bir
+						.RepeatForever()
+					)
+				);
+			});
+
+			services.AddQuartzHostedService(options =>
+			{
+				options.WaitForJobsToComplete = true;
+			});
+			services.AddScoped<ILiveChatHubService, LiveChatHubService>();
+            services.AddSignalR();
+            return services;
+        }
+    }
+}
