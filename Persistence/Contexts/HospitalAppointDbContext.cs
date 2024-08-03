ebdc8fa748cbc0c89ee22471d80450a1497ce9e9
Atacan Kilic <63428681+atcnk@users.noreply.github.com>
2024-08-03 16:04:12 +0300

    Added: db context for app

---
+++ Persistence/Contexts/HospitalAppointDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Core.Entities;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Persistence.EntityConfigurations;
+
+namespace Persistence.Contexts
+{
+    public class HospitalAppointDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public HospitalAppointDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<BaseUser> BaseUsers { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DoctorAvailability> DoctorAvailabilities { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientReport> PatientReports { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<SupportRequest> SupportRequests { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("Hospital-AppointConnectionString");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Remove cascade delete convention for one-to-many relationships
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.SetNull;
+            }
+
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new DepartmentConfiguration());
+            modelBuilder.ApplyConfiguration(new DoctorConfiguration());
+            modelBuilder.ApplyConfiguration(new OperationClaimConfiguration());
+            modelBuilder.ApplyConfiguration(new UserOperationClaimConfiguration());
+            modelBuilder.ApplyConfiguration(new DoctorAvailabilityConfiguration());
+            modelBuilder.ApplyConfiguration(new PatientConfiguration());
+            modelBuilder.ApplyConfiguration(new AppointmentConfiguration());
+            modelBuilder.ApplyConfiguration(new PatientReportConfiguration());
+            modelBuilder.ApplyConfiguration(new FeedbackConfiguration());
+            modelBuilder.ApplyConfiguration(new SupportRequestConfiguration());
+            modelBuilder.ApplyConfiguration(new NotificationConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
